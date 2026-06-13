53008a0f24b7f3929899deeef9b0ec56c34f9d7e
Rockey-Jason <jason.yeonwoo.im@gmail.com>
2026-06-13 12:54:37 +0900

    Update send-news.js

--- send-news.js
+++ send-news.js
@@ -1,1 +1,18 @@
-console.log("실행 성공");
+import { createClient }
+from "@supabase/supabase-js";
+
+import { Resend }
+from "resend";
+
+const supabase =
+createClient(
+process.env.SUPABASE_URL,
+process.env.SUPABASE_KEY
+);
+
+const resend =
+new Resend(
+process.env.RESEND_API_KEY
+);
+
+console.log("연결 성공");
